35ae425b76faf1eead29bbbf37e8b0218338667f
Thant Ko Zaw <thantmkou10@gmail.com>
2024-07-24 21:33:28 +0630

    things

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
-
-}
+        const node: Node<T> = { val: item };
+        this.length += 1;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+        if (!this.head) return undefined;
 
-}
+        this.length -= 1;
+        const h = this.head;
+        this.head = this.head.next;
+        h.next = undefined;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+        return h.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,39 @@
+type Node<T> = {
+    val: T;
+    prev?: Node<T>;
+};
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        const node: Node<T> = { val: item };
+        this.length += 1;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.prev = this.head;
+        this.head = node;
+    }
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.val;
+        }
 
-}
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+        return head?.val;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
