3ab51a79733cc150fa0e72df9c25f39e023edf9c
Thant Ko Zaw <thantmkou10@gmail.com>
2024-07-30 16:46:39 +0630

    linked list wip

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,106 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     prepend(item: T): void {
-
-}
+        const node: Node<T> = {
+            value: item,
+        };
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
     insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("invalid index");
+        } else if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
 
-}
+        const node: Node<T> = {
+            value: item,
+        };
+
+        let curr = this.head;
+        for (let i = 0; i < idx && curr; i++) {
+            curr = curr.next;
+        }
+        curr = curr as Node<T>;
+
+        node.next = curr;
+        node.prev = curr.prev;
+        curr.prev = node;
+
+        if (node.prev) {
+            node.prev.next = node;
+        }
+    }
     append(item: T): void {
-
-}
+        const node: Node<T> = { value: item };
+        this.length++;
+        if (!this.tail) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
+        this.tail.next = node;
+        node.prev = this.tail;
+        this.tail = node;
+    }
     remove(item: T): T | undefined {
+        let curr = this.head;
+        for (let i = 0; curr && i < this.length; i++) {
+            if (curr.value === item) {
+                break;
+            }
+            curr = curr.next;
+        }
+        if (!curr) {
+            return;
+        }
 
-}
+        this.length--;
+
+        if (this.length === 0) {
+            this.head = this.tail = undefined;
+            return;
+        }
+
+        curr.prev = 
+
+    }
     get(idx: number): T | undefined {
+        let curr = this.head;
+        for (let i = 0; i < idx && curr; i++) {
+            curr = curr.next;
+        }
 
-}
+        return curr?.value;
+    }
     removeAt(idx: number): T | undefined {
-
-}
+        return undefined;
+    }
 }
